d318dc8c72a0887172edfb3983d91477375ca0c6
cunhavcs <cunha.vcs@gmail.com>
2022-11-18 17:12:04 -0300

    Resolve requisito 03: Cria uma listagem de produtos

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import { searchCep } from './helpers/cepFunctions';
 import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
 import './style.css';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 
-fetchProductsList('computador');
+const productsList = await fetchProductsList('computador');
+
+productsList.forEach((product) => {
+  const products = document.querySelector('.products');
+  products.appendChild(createProductElement(product));
+});
