4c5ff066f3719767c48f0d4ab7d20d132b939ba8
cunhavcs <cunha.vcs@gmail.com>
2022-11-18 15:14:29 -0300

    Resolve requisito 02: Implementa a função fetchProductsList

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -2,6 +2,12 @@ export const fetchProduct = () => {
   // seu código aqui
 };
 
-export const fetchProductsList = () => {
-  // seu código aqui
+export const fetchProductsList = async (query) => {
+  if (query === undefined) {
+    throw new Error('Termo de busca não informado');
+  }
+  const url = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  return data.results;
 };
